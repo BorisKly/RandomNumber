ea1ef3d6183741ff044a6cecb18b306ff1ad3c6a
BorisKly <bklykavka@gmail.com>
2022-11-29 23:28:07 +0200

    add cache
    cache

--- NetworkManager/NetworkManager.swift
+++ NetworkManager/NetworkManager.swift
@@ -14,7 +14,7 @@ class NetworkManager {
 
     public static let shared = NetworkManager()
 
-    private let cache = NSCache<NSString, AnyObject>()
+    private let cache = NSCache<NSString, NSString>()
 
     private let urlSearch = "http://numbersapi.com/"
 
@@ -29,39 +29,27 @@ class NetworkManager {
         guard let url = URL(string: urlSearch + "\(dataR)?json=") else {
             return
         }
+        let key = NSString(string: dataR)
+
+        if let discription = cache.object(forKey: key) {
+            let viewData = NumberFactsData(text: discription as String , number: key.integerValue)
+            onSuccess(viewData)
+            print("!!!!!!!!!!!!!!!!!")
+            print(viewData)
+            print("data from cache")
+            return
+        }
         let task = URLSession.shared.dataTask(with: url ) { (data, _, _) in
             guard let data = data,
                   let jsonString = try? JSONDecoder().decode(NumberFactsResponseData.self, from: data) else {print("Error - cannot get information from url"); return}
             let viewData = NumberFactsData(
                 text: jsonString.text,
                 number: jsonString.number)
+
+            self.cache.setObject(viewData.text as NSString, forKey: String(viewData.number) as NSString)
+
             onSuccess(viewData)
         }
         task.resume()
      }
-
-//    public func fetchImage(imageCoverID: Int, size: Covers, onSuccess: @escaping (Data?) -> Void ) {
-//        let key = NSString(string: "\(imageCoverID) \(size.rawValue)")
-//        if let image = cache.object(forKey: key) {
-//            onSuccess(image.image)
-//            return
-//        }
-//
-//        let API = urlCover + String(imageCoverID) + size.rawValue
-//        guard  let apiURL = URL(string: API) else {
-//            onSuccess(nil)
-//            return
-//        }
-//        let task = URLSession.shared.dataTask(with: apiURL) { [weak self] (data, _, error) in
-//            guard let dataN =  data, error == nil else {
-//                onSuccess(nil)
-//                return
-//            }
-//            self?.cache.setObject(ImageData(dataN), forKey: key)
-//            onSuccess(dataN)
-//        }
-//        task.resume()
-//    }
-
 }
-
