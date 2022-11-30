6b3133dad98a4e3581dadabeb3b1935de6314c2b
BorisKly <bklykavka@gmail.com>
2022-11-30 20:02:03 +0200

    all Done
    all

--- NetworkManager/NetworkManager.swift
+++ NetworkManager/NetworkManager.swift
@@ -28,6 +28,7 @@ class NetworkManager {
     // MARK: - Public Methods
 
     public func resultOfNumberSearch(dataR: String, onSuccess: @escaping (NumberFactsData) -> Void, onError: (Error) -> Void) {
+
         guard let url = URL(string: urlSearch + "\(dataR)?json=") else {
             return
         }
@@ -38,9 +39,13 @@ class NetworkManager {
             onSuccess(viewData)
             return
         }
+
         let task = URLSession.shared.dataTask(with: url ) { (data, _, _) in
             guard let data = data,
-                  let jsonString = try? JSONDecoder().decode(NumberFactsResponseData.self, from: data) else {print("Error - cannot get information from url"); return}
+                  let jsonString = try? JSONDecoder().decode(NumberFactsResponseData.self, from: data) else {
+                print("Error - cannot get information from url");
+                return
+            }
             let viewData = NumberFactsData(
                 text: jsonString.text,
                 number: jsonString.number)
@@ -50,5 +55,8 @@ class NetworkManager {
             onSuccess(viewData)
         }
         task.resume()
+
+
      }
 }
+
